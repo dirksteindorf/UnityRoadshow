62d4caf2b6dedb9e0b8f7c9ef1b0a0541c7aa2e7
Svendamen <sven.schoenfeld@st.ovgu.de>
2014-11-09 06:29:19 +0100

    -- added : Banana movement         Collision detection         Face change

--- Assets/Scripts/BananaRise.cs
+++ Assets/Scripts/BananaRise.cs
@@ -4,17 +4,27 @@ using System.Collections;
 public class BananaRise : MonoBehaviour {
 	
 	public	float	speed		= 0;
+	public	float	normSpeed	= 0;
+	public	float	direction	= 0;
 	public	float	waitInit	= 0;
 	public	float	waitMid		= 0;
 	public	float	waitLast	= 0;
-	public	float	direction	= 0;
+	
+	public BoxCollider2D Blocker;
+
+	public Sprite	oldFace = null;
+	public Sprite	newFace = null;
+	public bool		lol		= false;
+
 
 	private	float	count		= 0;
 	private	float	waitTimer	= 0;
 	private	bool	texChng		= true;
 
-	Vector3 endpos ;
-	Vector3 startpos ;
+	Vector2 endpos;
+	Vector2 startpos;
+	Vector2 actPos;
+
 
 //	public bool LerpOut=false;
 //	public bool LerpIn=false;
@@ -23,33 +33,45 @@ public class BananaRise : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	
-		endpos		= this.transform.position + new Vector3(0, 1.5f, 0);
-		startpos	= this.transform.position;
+		startpos	= new Vector2 (this.transform.position.x, this.transform.position.y);
+		endpos		= startpos + new Vector2(0, .5f);
+		actPos		= startpos;
+		normSpeed 	= speed;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-//		if (LerpIn) {
-//			LerpIn=false;
-//			direction=-1;
-//				}
-//		if (LerpOut) {
-//			LerpOut=false;
-//			direction=1;
-//		}
+		// Determine current Position
+		actPos = new Vector2 (this.transform.position.x, this.transform.position.y);
 
-		if (direction <= 0 && this.rigidbody2D.transform.position == startpos)
+		// Wait in the Ground
+		if (direction <= 0 && actPos == startpos)
 		{
 			direction = 0;
 			if( (waitTimer += Time.deltaTime) >= waitInit )
 			{
+				speed = normSpeed;
 				waitTimer = 0;
 				direction = 1;
-				texChng = true;
+
+				if(	lol )
+					this.GetComponent<SpriteRenderer> ().sprite = oldFace;
 			}
 		} 
-		else if (this.rigidbody2D.transform.position == endpos)
+
+		// Calculate individual motion steps
+		count += Time.deltaTime * speed * direction;
+		count = Mathf.Max (0, Mathf.Min (1, count));
+
+		// Move Banana
+		this.rigidbody2D.transform.position = Vector2.Lerp (startpos, endpos, count);
+
+		// Determine current Position
+		actPos = new Vector2 (this.transform.position.x, this.transform.position.y);
+
+		// Wait Outside
+		if (actPos == endpos)
 		{
 			direction = 0;
 			if( (waitTimer += Time.deltaTime) >= (waitMid + waitLast) )
@@ -61,20 +83,39 @@ public class BananaRise : MonoBehaviour {
 			if( waitTimer >= waitMid && texChng )
 			{
 				// Change Sprite
-				texChng = false;
+				if(	lol )
+					this.GetComponent<SpriteRenderer> ().sprite = newFace;
+
 			}
-
-
 		}
 
-		count += Time.deltaTime * speed * direction;
-		count = Mathf.Max (0, Mathf.Min (1, count));
-		//Debug.Log (count);
-		//Debug.Log (direction);
 
-		this.rigidbody2D.transform.position = Vector2.Lerp (startpos, endpos, count);
-
-//		Debug.Log (waitTimer);
 	
 	}
+
+	// Whack the Banana
+	void OnMouseDown()
+	{
+		if (Blocker.bounds.Contains (Input.mousePosition))
+			return;
+
+		if (actPos != startpos)
+		{
+			speed		= 5;
+			direction	= -1;
+			waitTimer	= 0;
+
+			// Change Sprite
+			if(	lol )
+				this.GetComponent<SpriteRenderer> ().sprite = newFace;
+			texChng = false;
+		}
+
+
+	}
+
+
+
+
+
 }
